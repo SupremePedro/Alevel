74562a27867e5de835df7a0ea56ae20b2db3ceb5
petr <petrsupreme@gmail.com>
2020-01-06 21:26:32 +0200

    dsfdsf

---
+++ src/ua/com/alevel/classworks/lesson10/geometry/Circle.java
@@ -0,0 +1,18 @@
+package ua.com.alevel.classworks.lesson10.geometry;
+
+public class Circle extends Shape {
+    private int radius;
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return 0;
+    }
+}
--- src/ua/com/alevel/classworks/lesson10/geometry/Rectangle.java
+++ src/ua/com/alevel/classworks/lesson10/geometry/Rectangle.java
@@ -1,5 +1,32 @@
 package ua.com.alevel.classworks.lesson10.geometry;
 
 public class Rectangle extends Shape {
+    public void setHeight(double height) {
+        this.height = height;
+    }
 
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    private double height;
+    private double width;
+
+    Rectangle(double height, double width){
+        this.height = height;
+        this.width = width;
+    }
+
+    @Override
+    public double getArea() {
+        return getWidth()*getHeight();
+    }
 }
